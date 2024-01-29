21f04eed0705dbdcaec452a44417cc49d22a9b67
tomaselmgren <64467731+tomaselmgren@users.noreply.github.com>
2024-01-29 12:49:01 +0100

    added psql db and started building model, not yet tested

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'db.psql',
+        'OPTIONS': {
+            'host': "localhost",
+            'dbname': "test",
+            'user': "test",
+            'password': "test",
+            'port': "8080"
+        }
     }
 }
 
